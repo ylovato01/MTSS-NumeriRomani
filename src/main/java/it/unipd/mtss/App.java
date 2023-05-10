7a586d3786a25a9021eee380b8697dcfdd0796e2
ylovato01 <y.lovato01@gmail.com>
2023-05-10 12:40:24 +0200

    configurazione classi eseguita

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,5 +1,4 @@
-package it.unipd.mtss;
-
+package it.unipd.mtss; 
 /**
  * Hello world!
  *
@@ -9,5 +8,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        RomanPrinter rp=new RomanPrinter();
+        rp.print(3);
     }
 }
