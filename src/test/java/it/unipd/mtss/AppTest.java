ca13fb6b098a33a7ec2cf2908b8b1622ef917703
ylovato01 <y.lovato01@gmail.com>
2023-05-11 16:16:30 +0200

    test automatici

--- src/test/java/it/unipd/mtss/AppTest.java
+++ src/test/java/it/unipd/mtss/AppTest.java
@@ -12,9 +12,56 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+	
     @Test
-    public void shouldAnswerWithTrue()
+    public void testI()
     {
-        assertEquals("III",IntegerToRoman.convert(3));
+        assertEquals("I",IntegerToRoman.convert(1));
     }
+    @Test
+    public void testV()
+    {
+        assertEquals("V",IntegerToRoman.convert(5));
+    }
+    @Test
+    public void testX()
+    {
+        assertEquals("X",IntegerToRoman.convert(10));
+    }
+    @Test
+    public void testL()
+    {
+        assertEquals("L",IntegerToRoman.convert(50));
+    }
+    @Test
+    public void testC()
+    {
+        assertEquals("C",IntegerToRoman.convert(100));
+    }
+    @Test
+    public void testD()
+    {
+        assertEquals("D",IntegerToRoman.convert(500));
+    }
+    @Test
+    public void testM()
+    {
+        assertEquals("M",IntegerToRoman.convert(1000));
+    }
+    @Test
+    public void test888()
+    {
+        assertEquals("DCCCLXXXVIII",IntegerToRoman.convert(888));
+    }
+    @Test
+    public void testAsciiArt()
+    {
+        assertEquals("__   __ _       _____ __      __\r\n"
+        		  + "\\ \\ / /| |     |_   _|\\ \\    / /\r\n"
+        		  + " \\ V / | |       | |   \\ \\  / / \r\n"
+        		  + "  > <  | |       | |    \\ \\/ /  \r\n"
+        		  + " / . \\ | |____  _| |_    \\  /   \r\n"
+        		  + "/_/ \\_\\|______||_____|    \\/    ",RomanPrinter.print(44));
+    }
+    
 }
