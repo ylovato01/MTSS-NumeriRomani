3134a497e0c215e20e3f4ed81170ff19c2203635
ylovato01 <y.lovato01@gmail.com>
2023-05-11 12:31:24 +0200

    merge

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -7,8 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        RomanPrinter rp=new RomanPrinter();
-        rp.print(3);
+        System.out.println( RomanPrinter.print(44));      
     }
 }
