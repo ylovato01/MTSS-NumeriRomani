e35831903fc7a16a905bfa756a4c3507cb73faa7
Alcky26 <Alchy.26.07@gmail.com>
2023-05-15 14:59:09 +0200

    sistemato test ascii

--- src/test/java/it/unipd/mtss/AppTest.java
+++ src/test/java/it/unipd/mtss/AppTest.java
@@ -1,6 +1,9 @@
 package it.unipd.mtss;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter; 
 
 import org.junit.Test;
 
@@ -53,6 +56,40 @@ public class AppTest
     {
         assertEquals("DCCCLXXXVIII",IntegerToRoman.convert(888));
     }
-    
+    @Test
+    public void testAsciiArt()
+    {
+    	try {
+    	      File myObj = new File("filename.txt");
+    	      if (myObj.createNewFile()) {
+    	        System.out.println("File created: " + myObj.getName());
+    	      } else {
+    	        System.out.println("File already exists.");
+    	      }
+    	      FileWriter myWriter = new FileWriter("filename.txt");
+    	        myWriter.write("__   __ _       _____ __      __\r\n"
+    	        		+ "\\ \\ / /| |     |_   _|\\ \\    / /\r\n"
+    	        		+ " \\ V / | |       | |   \\ \\  / / \r\n"
+    	        		+ "  > <  | |       | |    \\ \\/ /  \r\n"
+    	        		+ " / . \\ | |____  _| |_    \\  /   \r\n"
+    	        		+ "/_/ \\_\\|______||_____|    \\/    \r\n"
+    	        		+ "\n"+RomanPrinter.print(44));
+    	        myWriter.close();
+    	        System.out.println("Successfully wrote to the file.");
+    	    } catch (IOException e) {
+    	      System.out.println("An error occurred.");
+    	      e.printStackTrace();
+    	      System.out.println("An error occurred.");
+    	        e.printStackTrace();
+    	    }
+        
+        assertEquals("__   __ _       _____ __      __\n"
+        		+ "\\ \\ / /| |     |_   _|\\ \\    / /\n"
+        		+ " \\ V / | |       | |   \\ \\  / / \n"
+        		+ "  > <  | |       | |    \\ \\/ /  \n"
+        		+ " / . \\ | |____  _| |_    \\  /   \n"
+        		+ "/_/ \\_\\|______||_____|    \\/    \n"
+        		+ "",RomanPrinter.print(44));
+    }
     
 }
